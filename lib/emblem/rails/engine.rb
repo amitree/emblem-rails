add6b08e60a80e24fcf34325ca3f209150ebdded
Tony Novak <tony@amitree.com>
2016-01-15 14:45:59 -0500

    Passing mime_type to fix encoding issues

--- lib/emblem/rails/engine.rb
+++ lib/emblem/rails/engine.rb
@@ -5,7 +5,7 @@ module Emblem
     class Engine < ::Rails::Engine
       initializer "ember_rails.setup", :after => :append_assets_path, :group => :all do |app|
         app.config.assets.configure do |env|
-          env.register_engine '.emblem', Emblem::Rails::Template
+          env.register_engine '.emblem', Emblem::Rails::Template, mime_type: 'text/plain'
         end
       end
     end
